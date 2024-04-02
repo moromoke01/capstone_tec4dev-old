8a7717cc104d05c5d33e4deb56ff897533ef6db1
memunasaani <memunasaani1995@gmail.com>
2024-04-02 17:32:53 +0000

    new change

--- new123/src/App.js
+++ new123/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import './Components/Navbar'
+import Result from './Components/Result';
+//import Navbar from './Components/Navbar';
 //import Home from './Components/Home'
 //import Navbar from './Components/Navbar';
-//import Result from './Components/Result';
 //import Quiz from './Components/quiz/quiz';
 function App() {
   return (
     <div className="App">
-      
+
       <Result/>
     </div>
   );
