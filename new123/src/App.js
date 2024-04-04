035d33ba6a8b3ad2390959984534cfe94849c2c2
memunasaani <memunasaani1995@gmail.com>
2024-04-04 04:21:34 +0000

    working on rounting

--- new123/src/App.js
+++ new123/src/App.js
@@ -1,15 +1,31 @@
+import React from 'react';
+//import { ReactDOM } from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './App.css';
 import './Components/Navbar'
 import Result from './Components/Result';
-//import Navbar from './Components/Navbar';
-//import Home from './Components/Home'
-//import Navbar from './Components/Navbar';
-//import Quiz from './Components/quiz/quiz';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home'
+import Quiz from './Components/quiz/quiz';
 function App() {
+  const router=createBrowserRouter([{
+    path:'/',
+    element:<Navbar/>,
+    element: <Home/>,
+    errorElement: <div>Not Found</div>
+  },
+  {
+    path: '/result',
+    element: <Result/>
+  },
+  {
+    path:'/quiz',
+    element: <Quiz/>
+  }
+])
   return (
     <div className="App">
-
-      <Result/>
+      <RouterProvider router={router}/>
     </div>
   );
 }
