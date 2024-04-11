8b271958ec59c6fbc05d57fab827dcb5a5363ca6
Mojisola-Akinrelere <mojisolaoakinrelere@gmail.com>
2024-04-11 19:13:37 +0100

    updated sign up and sign in page

--- new123/src/Components/LoginSignup/LoginSignup.js
+++ new123/src/Components/LoginSignup/LoginSignup.js
@@ -20,18 +20,26 @@ const SignupForm = ({ setAction }) => (
         <div className="underline"></div>
       </div>
       <div className="signup-heading">
-        <h2>Sign up to take your career psychometric test</h2>
+        <h4>Sign up to take your career psychometric test</h4>
       </div>
       <div className="inputs">
         <InputField label="Name" type="text" name="name" placeholder="Enter your name" />
         <InputField label="Email" type="email" name="email" placeholder="Enter your email" />
         <InputField label="Password" type="password" name="password" placeholder="Enter your password" />
       </div>
-      <div className="already-have-account">
-        Already have an account? <Link to="/signin" onClick={() => setAction("Login")}>Sign in</Link>
+
+      {/* Checkbox for agreeing to terms */}
+      <div className="agree-checkbox">
+        <input type="checkbox" id="agreeTerms" name="agreeTerms" />
+        <label htmlFor="agreeTerms">I agree with Insightify's <Link to="/terms">terms of service</Link>, <Link to="/privacy">privacy policy</Link>, and default settings</label>
       </div>
+    
       <div className="submit-container">
         <button className="submit" onClick={() => {/* Handle sign up submission */}}>SIGN UP</button>
+      </div>
+
+      <div className="already-have-account">
+        Already have an account? <Link to="/signin" onClick={() => setAction("Login")}>Sign in</Link>
       </div>
     </div>
   </div>
@@ -46,18 +54,17 @@ const SigninForm = () => (
         <div className="underline"></div>
       </div>
       <div className="signin-heading">
-        <h2>Sign in to stay connected with us and continue to explore</h2>
+        <h5>Sign in to stay connected with us and continue to explore</h5>
       </div>
       <div className="inputs">
         <InputField label="Email" type="email" name="email" placeholder="Enter your email" />
         <InputField label="Password" type="password" name="password" placeholder="Enter your password" />
       </div>
-      <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
+      <div className="forgot-password">Forgot Password?</div>
       <div className="submit-container">
         <button className="submit" onClick={() => {/* Handle sign in submission */}}>SIGN IN</button>
       </div>
     </div>
   </div>
 );
-
 export { SignupForm, SigninForm };
